840e7d5d8eda68e22f1d9b94302a4ba32691c68d
Maria Angelica Castaño <mcastanoal@edu.co>
2025-11-26 21:15:25 -0500

    cristiannn tqm x2 pq me equivoque jeje <3

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from chatbot import get_response
 import base64
 import time
 
-# ================= IMÁGENES BASE64 =================
 def load_image_base64(path):
     with open(path, "rb") as f:
         return base64.b64encode(f.read()).decode()
@@ -13,7 +12,7 @@ user_pic64 = load_image_base64("Imagenes/Perfil.jpg")
 
 st.set_page_config(page_title="Jerry", layout="centered")
 
-# =============== FONDO DEGRADADO ===============
+
 st.markdown("""
 <style>
     .stApp {
@@ -21,12 +20,12 @@ st.markdown("""
         background-attachment: fixed;
     }
 
-    /* SCROLL */
+    
     .chat-scroll{max-height:420px;overflow-y:auto;padding:10px;}
     .chat-scroll::-webkit-scrollbar{width:7px;}
     .chat-scroll::-webkit-scrollbar-thumb{background:#7792aa;border-radius:10px;}
 
-    /* BURBUJAS */
+    
     .bot-msg{background:#fff;padding:12px 16px;border-radius:15px 15px 15px 5px;
              border-left:4px solid #335c81;font-size:16px;max-width:60%;
              box-shadow:0 2px 6px rgba(0,0,0,.08)}
@@ -41,7 +40,6 @@ st.markdown("""
             background:rgba(255,255,255,.06);backdrop-filter:blur(6px);text-align:center;}
     .header img{width:60px;height:60px;border-radius:50%;margin-bottom:6px;border:2px solid #ffffff80;}
 
-    /* ---- TYPING BOX ---- */
     .typing-box{background:white;padding:10px 15px;border-radius:18px;display:flex;align-items:center;gap:7px;}
     .dot{width:7px;height:7px;background:#777;border-radius:50%;animation:blink 1.3s infinite;}
     .dot:nth-child(2){animation-delay:.2s;}
@@ -50,7 +48,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# ================= ENCABEZADO =================
+
 st.markdown(f"""
 <div class="header">
     <img src="data:image/jpeg;base64,{bot_pic64}">
@@ -59,10 +57,10 @@ st.markdown(f"""
 </div>
 """, unsafe_allow_html=True)
 
-# ================= CONTENEDOR FIJO PARA "JERRY ESTÁ ESCRIBIENDO" =================
+
 typing_top = st.empty()
 
-# ================= SISTEMA DEL CHAT =================
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -88,18 +86,17 @@ with chat:
 
     st.markdown("</div>", unsafe_allow_html=True)
 
-# ================= INPUT + BOTÓN =================
+
 col1,col2 = st.columns([8,2])
 with col1:
     user = st.text_input("", placeholder="Escribe aquí...", key="input", label_visibility="collapsed")
 with col2:
     send = st.button("Enviar", use_container_width=True)
 
-# ================= PROCESAR MENSAJE + TYPING INDICATOR =================
+
 def procesar():
     st.session_state.messages.append({"role":"user","content":user})
 
-    # 🔥 ⬆ TYPING EN LA PARTE SUPERIOR DEL CHAT ⬆ 🔥
     typing_top.markdown(f"""
     <div style='margin-bottom:10px;display:flex;justify-content:center;'>
         <div class='typing-box'>
